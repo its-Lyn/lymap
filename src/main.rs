8f4a0230601a44cc93a42ac08f8deaa5dee2a9be
Lyn <itslyn.dev@gmail.com>
2024-01-20 00:25:23 +0200

    Create a very broken file dialog thing... (its never been more over than it is now)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use std::borrow::Borrow;
 
 use config::{create::create_config, models::{WindowConfig, ButtonConfig}};
 
 use device_query::{DeviceState, Keycode, DeviceQuery};
 
 use env_logger::Builder;
-use log::{error, warn, LevelFilter};
+use log::{error, warn, LevelFilter, info};
 
-use macroquad::{window::{next_frame, clear_background, request_new_screen_size}, text::{draw_text, load_ttf_font, draw_text_ex, TextParams}};
-use macroquad::color;
+use macroquad::{miniquad, window::{next_frame, clear_background, request_new_screen_size}, text::load_ttf_font, input::{is_key_down, is_key_released}};
 
+use native_dialog::FileDialog;
 use ui::buttons;
 use utils::{colour::hex_to_rgb, config_path::get_config_path};
 
@@ -70,9 +69,9 @@ async fn main() {
     }
 
     let bg_colour = hex_to_rgb(&window_config.bg_colour);
+    let mut btn_config: Option<ButtonConfig> = Default::default();
 
-    let btn_source = std::fs::read_to_string("/home/lyn/.config/lymap/layouts/test_layout.json").unwrap();
-    let btn_config: ButtonConfig = serde_json::from_str(&btn_source).unwrap();
+    let mut config_load = false;
 
     let device_state = DeviceState::new(); 
     
@@ -83,11 +82,61 @@ async fn main() {
         // Update
         let keys: Vec<Keycode> = device_state.get_keys();
 
+        if is_key_down(miniquad::KeyCode::LeftControl) && is_key_down(miniquad::KeyCode::L) && !config_load {
+            info!("Trying to load config folder...");
+        
+            let path = FileDialog::new()
+                .set_location(&format!("{}/layouts", get_config_path()))
+                .add_filter("JSON File", &["json"])
+                .show_open_single_file();
+
+            match path {
+                Ok(val) => {
+                    if let Err(e) = val {
+                        error!("Failed to parse string: {}", e);
+                        std::process::exit(1);
+                    }
+                    
+                    match std::fs::read_to_string(val) {
+                        Ok(source) => {
+                            match serde_json::from_str(&source) {
+                                Ok(config) => {
+                                    btn_config = config;
+                                },
+
+                                Err(e) => {
+                                    error!("Failed to deserialise config: {}", e);
+                                    std::process::exit(1);
+                                } 
+                            }
+                        },
+
+                        Err(e) => {
+                            error!("Failed to parse JSON source: {}", e);
+                            std::process::exit(1);
+                        }
+                    }
+
+                },
+
+                Err(e) => {
+                    error!("Failed to initialise file dialog: {}", e);
+                    std::process::exit(1);
+                }
+            }
+
+            config_load = true;
+        } else if is_key_released(miniquad::KeyCode::LeftControl) || is_key_released(miniquad::KeyCode::L) {
+            config_load = false;
+        }
+
         // Draw
         clear_background(bg_colour);
-
-        for button in btn_config.buttons.iter() { 
-             buttons::draw_fmt(button, &font, &keys, &btn_config.idle_colour, &btn_config.active_colour);
+        
+        if let Some(config) = &btn_config { 
+            for button in config.buttons.iter() { 
+                buttons::draw_fmt(button, &font, &keys, &config.idle_colour, &config.active_colour);
+            }
         }
 
         // Advance frame
